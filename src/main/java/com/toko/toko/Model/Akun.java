15c343e501ad7ba5cd0f2cad2d5eedb1a615b5ee
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-02-05 13:58:15 +0700

    add model
    upgrade depedencies dan membuatm odel

---
+++ src/main/java/com/toko/toko/Model/Akun.java
@@ -0,0 +1,27 @@
+package com.toko.toko.Model;
+
+public class Akun {
+    private String username;
+    private String password;
+
+    public Akun(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/toko/toko/Model/Hutang.java
@@ -0,0 +1,66 @@
+package com.toko.toko.Model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.StringProperty;
+
+public class Hutang {
+    private StringProperty nama;
+    private StringProperty alamat;
+    private StringProperty tanggal_hutang;
+    private DoubleProperty jumlah_hutang;
+
+    public Hutang(String nama, String alamat, String tanggal_hutang, double jumlah_hutang) {
+        setNama(nama);
+        setAlamat(alamat);
+        setTanggal_hutang(tanggal_hutang);
+        setJumlah_hutang(jumlah_hutang);
+    }
+
+    public String getNama() {
+        return nama.get();
+    }
+
+    public StringProperty namaProperty() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama.set(nama);
+    }
+
+    public String getAlamat() {
+        return alamat.get();
+    }
+
+    public StringProperty alamatProperty() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat.set(alamat);
+    }
+
+    public String getTanggal_hutang() {
+        return tanggal_hutang.get();
+    }
+
+    public StringProperty tanggal_hutangProperty() {
+        return tanggal_hutang;
+    }
+
+    public void setTanggal_hutang(String tanggal_hutang) {
+        this.tanggal_hutang.set(tanggal_hutang);
+    }
+
+    public double getJumlah_hutang() {
+        return jumlah_hutang.get();
+    }
+
+    public DoubleProperty jumlah_hutangProperty() {
+        return jumlah_hutang;
+    }
+
+    public void setJumlah_hutang(double jumlah_hutang) {
+        this.jumlah_hutang.set(jumlah_hutang);
+    }
+}
