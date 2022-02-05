1a867b08596cc1023a39002b55b9646a66511c1c
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-02-05 13:36:37 +0700

    make new scene

--- src/main/java/com/toko/toko/Controller/ControlLogin.java
+++ src/main/java/com/toko/toko/Controller/ControlLogin.java
@@ -2,39 +2,30 @@ package com.toko.toko.Controller;
 
 import com.toko.toko.Main;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ControlLogin implements Initializable {
 
-
-
-    @FXML
-    protected void handleLoginAction(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Main.class.getResource("HomePage.fxml"));
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Toko Febrian");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/shop.png")));
-        stage.show();
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void handleLoginAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("HomePage.fxml")));
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
