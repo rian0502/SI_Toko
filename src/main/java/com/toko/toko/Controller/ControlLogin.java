25dfe127891ffb97282f801b7d6067d05041a261
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-02-05 15:20:10 +0700

    control login

--- src/main/java/com/toko/toko/Controller/ControlLogin.java
+++ src/main/java/com/toko/toko/Controller/ControlLogin.java
@@ -1,6 +1,8 @@
 package com.toko.toko.Controller;
 
 import com.toko.toko.Main;
+import com.toko.toko.Model.Akun;
+import com.toko.toko.Model.DatabaseModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,10 +13,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import javax.swing.*;
 
 public class ControlLogin implements Initializable {
 
@@ -22,17 +24,34 @@ public class ControlLogin implements Initializable {
     public TextField tfUsername;
     @FXML
     public PasswordField tfPassword;
-
+    DatabaseModel dbm;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        dbm = new DatabaseModel();
     }
 
-    public void handleLoginAction(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("HomePage.fxml")));
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    public void handleLoginAction(ActionEvent event) {
+        try {
+            if(dbm.loginAkun(new Akun(tfUsername.getText().trim(), tfPassword.getText().trim()))){
+                tfUsername.clear();
+                tfPassword.clear();
+                Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("HomePage.fxml")));
+                Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }else {
+                JOptionPane.showMessageDialog(null, "Username Atau Password Salah","Login",JOptionPane.ERROR_MESSAGE);
+                tfUsername.clear();
+                tfPassword.clear();
+            }
+        }catch (Exception e){
+            System.out.println("EROR login");
+            System.out.println(e.getMessage());
+        }
+
+
+
+
     }
 }
